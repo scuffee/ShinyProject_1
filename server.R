b192d43df89d5165fb0191a31f482270b4d83f85
scuffee <sc2714@att.com>
2015-12-12 10:13:00 -0600

    update .R files

--- server.R
+++ server.R
@@ -1,25 +1,28 @@
-# 35000 calories = 10 pound
+
 
 weightLoss <- function(pounds, calorie)
 {
-  x = 0   #Calorie reduction accumulator
-  prediction = 0   #Number of days counter
+  x = 0   #Calorie reduction counter
+  days = 0   #Number of days counter
   totalCalories = pounds * 3500 #Number of calores needed to reduce desired pounds
   while (x < totalCalories) #Calculating the number of days required
       {
           x = x + calorie
-          prediction = prediction + 1
+          days = days + 1
       }
   
- prediction #Total number of days
+
+ days #Total number of days
  
 }
 
 shinyServer(
-  function(input,input1, output){
+  function(input,input1, output, output1){
     output$inputValue1 <- renderPrint({input$pounds})
     output$inputValue2 <- renderPrint({input$calorie})
-    output$prediction <- renderPrint({weightLoss(input$pounds,input$calorie)})
+    output$days <- renderPrint({weightLoss(input$pounds,input$calorie)})
+    
+    
     
     
       
--- ui.R
+++ ui.R
@@ -2,20 +2,20 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("Weight loss management plan"),
   sidebarPanel(
-    numericInput('pounds', 'Number of pounds you would like to lose up to a maximum of 50 lbs', 10, min = 5, max = 50, step= 5),
+    numericInput('pounds', 'Number of pounds you would like to lose up to a maximum of 50', 10, min = 5, max = 50, step= 5),
     submitButton('Submit'),
-    numericInput('calorie', 'Daily calorie reduction', 100, min = 100, max = 800, step = 50),
+    numericInput('calorie', 'Daily calorie reduction up to a maximum of 800', 100, min = 100, max = 800, step = 50),
     submitButton('Submit')
     
   ),
   mainPanel(
-    h3('Results of prediction'),
-    h4('The amount of weight you want to lose is:'),
+    h3('Results of th plan'),
+    h4('The number of pounds you want to lose is:'),
     verbatimTextOutput("inputValue1"),
     h4('The planned daily calorie reduction is:'),
     verbatimTextOutput("inputValue2"),
-    h4('Which resulted in the predicition of'),
-    verbatimTextOutput("prediction"),
+    h4('Which requires:'),
+    verbatimTextOutput("days"),
     h4('days.')
   )
   
